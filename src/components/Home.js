20cd7c4e68cd87b3428354d15b8a3fd135ec31eb
mendelB <mendelblesofsky@gmail.com>
2017-01-30 18:44:28 -0500

    add clickHandler and state

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,20 @@ import '../css/Home.css';
 import {Button, FormGroup, FormControl} from 'react-bootstrap';
 
 class Home extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			value: ''
+		};
+		this.handleChange = this.handleChange.bind(this);
+		this.handleClick = this.handleClick.bind(this);
+	}
+	handleChange(event) {
+		this.setState({value: event.target.value});
+	}
+	handleClick() {
+		console.log(this.state.value)
+	}
 	render() {
 		return (
 			<div className="Home">
@@ -10,10 +24,10 @@ class Home extends Component {
 					<h1>Enter a City and State</h1>
 					<div className="Input">
 						<FormGroup bsSize="large">
-	      					<FormControl type="text" placeholder="New York, NY" />
+	      					<FormControl type="text" placeholder="New York, NY" value={this.state.value} onChange={this.handleChange} />
 	    				</FormGroup>
 	    			</div>
-					<Button bsStyle="success" bsSize="large">Get Weather</Button>
+					<Button bsStyle="success" bsSize="large" onClick={this.handleClick}>Get Weather</Button>
 				</div>
 			</div>
 		);
