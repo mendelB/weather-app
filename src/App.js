9ead0a14ed576a2fcb7d72b3200b5c6a0a5a5a39
mendelB <mendelblesofsky@gmail.com>
2017-01-30 18:21:19 -0500

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react';
+import {Button, Form, FormGroup, FormControl, Navbar, Nav, NavItem} from 'react-bootstrap';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-          <h2>Welcome to React</h2>
-
+        <Navbar className="MyNav">
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="#">Weather App</a>
+            </Navbar.Brand>
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav pullRight>
+              <NavItem>
+                <Form inline className="Form">
+                  <FormGroup controlId="formInlineName">
+                    <FormControl type="text" placeholder="New York, NY" />
+                    {' '}
+                    <Button bsStyle="success" bsSize="small">Get Weather</Button>
+                  </FormGroup>
+                </Form>
+              </NavItem>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
         { this.props.children }
       </div>
     );
