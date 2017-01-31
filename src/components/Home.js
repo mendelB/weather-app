0531bde4907fda257f0534e34078919a6c1c4e4e
mendelB <mendelblesofsky@gmail.com>
2017-01-30 20:36:54 -0500

    basic weather fetch functionality

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import WeatherFetcher from '../utils/WeatherFetcher'
 import '../css/Home.css';
 import {Button, FormGroup, FormControl} from 'react-bootstrap';
 
@@ -6,7 +7,9 @@ class Home extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			value: ''
+			value: '',
+			data: '',
+			loaded: true
 		};
 		this.handleChange = this.handleChange.bind(this);
 		this.handleClick = this.handleClick.bind(this);
@@ -15,10 +18,13 @@ class Home extends Component {
 		this.setState({value: event.target.value});
 	}
 	handleClick() {
-		console.log(this.state.value)
+		WeatherFetcher.fetchWeather(this.state.value)
+			.then((data) => this.setState({data: data.data, loaded: false}));
 	}
 	render() {
 		return (
+			this.state.loaded
+			?
 			<div className="Home">
 				<div className="Instructions">
 					<h1>Enter a City and State</h1>
@@ -29,6 +35,11 @@ class Home extends Component {
 	    			</div>
 					<Button bsStyle="success" bsSize="large" onClick={this.handleClick}>Get Weather</Button>
 				</div>
+			</div>
+			:
+			<div>
+				<h4>The weather is:</h4>
+				<p>{this.state.data.weather[0].description}</p>
 			</div>
 		);
 	}
