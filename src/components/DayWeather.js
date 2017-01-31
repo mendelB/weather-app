6c45cc944db125afba349deff32d284c4f672ff7
mendelB <mendelblesofsky@gmail.com>
2017-01-31 17:38:59 -0500

    pretty much done main 5 day view

--- src/components/DayWeather.js
+++ src/components/DayWeather.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
+import {Image, Col, Thumbnail, Button} from 'react-bootstrap';
 import '../css/DayWeather.css';
 
 class DayWeather extends Component {
 	render() {
 		return (
-			<div className="DayWeather">
-				<img className={this.props.icon} alt="weather icon"/>
-				<p>{this.props.date}</p>
-			</div>
+			<Col xs={6} md={3} className="DayWeather">
+				<Image className="img" src={process.env.PUBLIC_URL + '/weather-icons/' + this.props.icon + '.svg'}  alt="542x500" />
+				<h3 className="Date">{this.props.date}</h3>
+			</Col>
 		)
 	}
 }
--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import moment from 'moment'
 import WeatherFetcher from '../utils/WeatherFetcher';
+import {Grid, Row} from 'react-bootstrap';
 import DayWeather from './DayWeather';
 import '../css/Forecast.css';
 
@@ -23,16 +24,29 @@ class Forecast extends Component {
 		let weathers = this.state.loaded && this.state.fiveDayWeather.list.map((w, i) => 
 			<DayWeather 
 				date={moment().add(i, 'day').format('LL')} 
-				icon={w.weather[0].main.toLowerCase()}
+				icon={w.weather[0].icon.toLowerCase()}
 				key={i} />
 		)
+		weathers.length
 		return (
 			this.state.loaded
 			?
 				<div className="Forecast">
 					<h1>{this.state.fiveDayWeather.city.name}</h1>
 					<h2>Select a Day</h2>
-					{weathers} 
+					<Grid>
+					    <Row>
+							{weathers[0]}
+							{weathers[1]}
+							{weathers[2]}
+							{weathers[3]} 
+					    </Row>
+					</Grid>
+					<Grid>
+						<Row>
+							{weathers[4]}
+						</Row>
+					</Grid>
 					<button onClick={this.clickHandle}>Click</button>
 				</div>
 			:
